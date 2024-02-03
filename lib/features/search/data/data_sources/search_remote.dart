5888315572cbff23c15cb427af9c30520039837a
kimiya zargari <kimiyazargari13@gmail.com>
2024-02-03 09:56:03 +0330

    cancel previous request before sending a new one

--- lib/features/search/data/data_sources/search_remote.dart
+++ lib/features/search/data/data_sources/search_remote.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flickr_search_images/core/network_manager/api_routes.dart';
 import 'package:flickr_search_images/core/network_manager/http_service.dart';
 import 'package:flickr_search_images/features/search/data/models/search_params.dart';
@@ -6,12 +7,19 @@ import 'package:injectable/injectable.dart';
 @singleton
 class SearchRemoteDataSource {
   final HttpService httpService;
+  CancelToken? cancelToken;
 
   SearchRemoteDataSource(this.httpService);
 
   Future<HttpClientResponse> searchImage(SearchParams searchParams) async {
+    ///cancels previous request before sending a new one
+    if (cancelToken != null) {
+      cancelToken!.cancel();
+    }
+    cancelToken = CancelToken();
     return httpService.get(
       ApiRoutes.baseUrl,
+      cancelToken: cancelToken,
       queryParameters: searchParams.toJson()
         ..addAll({'method': 'flickr.photos.search'}),
     );
